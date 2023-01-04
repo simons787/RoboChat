dcd273e3be4b1379494965cc04aa757b69fc1d3b
SimonS <96791557+simons787@users.noreply.github.com>
2023-01-04 14:23:58 +0100

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,21 @@
+#Python Authomatic chat
+#Simone Sabatti
+#cretaed: 4 january 2023
+
+#Says hello
+print("Hello !")
+#Ask your name
+print("What's your name ?")
+myName = input()
+#Prints a custom welcome message
+print("Nice to meet you " + myName + " !")
+#says the lenght of your name
+print("The lenght of your name is of: " + str(len(myName)) + " Characters" )
+#Asks how old are you
+print("How old are you ?")
+myAge = input()
+print("And so you are " + myAge)
+#Says how old you will be in 1 year
+print("Do You know that you will be " + str(int(myAge) + 1) + " the next year ?")
+
+
